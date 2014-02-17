92898a69fa65ab4a5d725117a4e1d178d1185613
Alexander Böhn <fish2000@gmail.com>
2014-02-17 09:52:31 -0500

    Corrected the accedentally totally incorrect RGB-to-Y-value coefficients in grayscale.pyx

--- pylire/process/ext/grayscale.pyx
+++ pylire/process/ext/grayscale.pyx
@@ -11,9 +11,9 @@ cdef inline FLOAT32_t sG_NUMERATOR = <FLOAT32_t>587.0
 cdef inline FLOAT32_t sB_NUMERATOR = <FLOAT32_t>114.0
 cdef inline FLOAT32_t ONETHOUSAND = <FLOAT32_t>1000.0
 
-cdef inline FLOAT32_t POINTTWOONENINE = <FLOAT32_t>0.219
-cdef inline FLOAT32_t POINTFIVEEIGHTSEVEN = <FLOAT32_t>0.219
-cdef inline FLOAT32_t POINTONEFOURTEEN = <FLOAT32_t>0.219
+cdef inline FLOAT32_t POINTTWONINENINE = <FLOAT32_t>0.299
+cdef inline FLOAT32_t POINTFIVEEIGHTSEVEN = <FLOAT32_t>0.587
+cdef inline FLOAT32_t POINTONEFOURTEEN = <FLOAT32_t>0.114
 cdef inline FLOAT32_t TWOONENINE = <FLOAT32_t>219.0
 cdef inline FLOAT32_t TWOFIFTYSIX = <FLOAT32_t>256.0
 cdef inline FLOAT32_t SIXTEENPOINTFIVE = <FLOAT32_t>16.5
@@ -37,6 +37,6 @@ def YValue(numpy.ndarray[FLOAT32_t, ndim=2] sR not None,
            numpy.ndarray[FLOAT32_t, ndim=2] sB not None):
     return \
         (TWOONENINE * (
-            (POINTTWOONENINE * sR + POINTFIVEEIGHTSEVEN * sG + POINTONEFOURTEEN * sB) / TWOFIFTYSIX
+            (POINTTWONINENINE * sR + POINTFIVEEIGHTSEVEN * sG + POINTONEFOURTEEN * sB) / TWOFIFTYSIX
             ).astype('double') + SIXTEENPOINTFIVE
         ).astype('int')
