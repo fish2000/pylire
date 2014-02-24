0ab25ffd6029bd549b55d0ea626cdd5755cdf96a
Alexander Böhn <fish2000@gmail.com>
2014-02-24 07:51:49 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 
 setup(
     name='pylire',
-    version='0.2.8',
+    version='0.2.9',
     description='Python/Cython port of Lire image processing algorithms',
     author='Alexander Bohn',
     author_email='fish2000@gmail.com',
