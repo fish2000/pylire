a3bd41a1cf1c194230472716f3334df43eb9d3e2
Alexander Böhn <fish2000@gmail.com>
2014-06-11 19:47:27 -0700

    ColorLayout needs only fdct() and cl_base64() to be good-to-go

--- pylire/process/color_layout.py
+++ pylire/process/color_layout.py
@@ -5,10 +5,14 @@ import numpy
 import numexpr
 
 from pylire.process.channels import YCbCr
+from pylire.process.bitsampling import histogram_hash, histogram_hash_string
 
 SHAPE_WIDTH = 8
 SHAPE_HEIGHT = 8
 SHAPE_SIZE = SHAPE_WIDTH * SHAPE_HEIGHT
+
+NUM_Y = 21
+NUM_C = 6
 
 zigzag = numpy.array([
     0, 1, 8, 16, 9, 2, 3, 10, 17, 24, 32, 25, 18, 11, 4, 5,
@@ -69,28 +73,34 @@ def k_map(ndim):
 
 def quant_ydc(ints):
     return numexpr.evaluate("""
-where(ints > 192, 112 + ((i - 192) >> 2),
-where(ints > 160, 96 + ((i - 160) >> 1),
-where(ints > 96, 32 + (i - 96),
-where(ints > 64, 16 + ((i - 64) >> 1), 0))))""")
+where(ints > 192, 112 + ((ints - 192) >> 2),
+where(ints > 160, 96 + ((ints - 160) >> 1),
+where(ints > 96, 32 + (ints - 96),
+where(ints > 64, 16 + ((ints - 64) >> 1), 0))))""")
 
 def quant_cdc(ints):
     return numexpr.evaluate("""
 where(ints > 191, 63,
-where(ints > 160, 56 + ((i - 160) >> 2),
-where(ints > 144, 48 + ((i - 144) >> 1),
-where(ints > 112, 16 + (i - 112),
-where(ints > 96, 8 + ((i - 96) >> 1),
-where(ints > 64, (i - 64) >> 2, 0))))))""")
+where(ints > 160, 56 + ((ints - 160) >> 2),
+where(ints > 144, 48 + ((ints - 144) >> 1),
+where(ints > 112, 16 + (ints - 112),
+where(ints > 96, 8 + ((ints - 96) >> 1),
+where(ints > 64, (ints - 64) >> 2, 0))))))""")
 
 def quant_ac(ints):
     ints = numpy.clip(ints, -256, 255)
+    nints = numpy.absolute(ints)
     out = numexpr.evaluate("""
-where(abs(ints) > 127, 64 + ((abs(ints)) >> 2),
-where(abs(ints) > 63, 32 + ((abs(ints)) >> 2), abs(ints)
+where(nints > 127, 64 + ((nints) >> 2),
+where(nints > 63, 32 + ((nints) >> 2), nints
 ))""")
     signed = numexpr.evaluate("where(ints < 0, -1, 1)")
+    if not nints.shape:
+        return None
     return (out * signed) + 128
+
+def fdct(shape):
+    pass
 
 def shape_from_image(ndim):
     """ Return a 'shape' square for a given RGB image.
@@ -122,8 +132,51 @@ def shape_from_image(ndim):
             continue
         Shape[kidx] = (KChannelSums[kidx] / KCounts[kidx]).astype('int')
 
-    return Shape
+    return Shape.T
 
+def color_layout(ndim):
+    shape = shape_from_image(ndim)
+    YCoeff = numpy.zeros((64,), dtype="int")
+    CbCoeff = numpy.zeros((64,), dtype="int")
+    CrCoeff = numpy.zeros((64,), dtype="int")
+    
+    fdct(shape.T[0].T) # or something
+    fdct(shape.T[1].T)
+    fdct(shape.T[2].T)
+    
+    # print("ZIGZAG:")
+    # print(shape[0][zigzag] >> 1)
+    # print("NORMAL:")
+    # print(shape[0] >> 1)
+    
+    YCoeff = quant_ac(shape[0][zigzag] >> 1) >> 3
+    CbCoeff = quant_ac(shape[1][zigzag] >> 3)
+    CrCoeff = quant_ac(shape[2][zigzag] >> 3)
+    
+    YCoeff[0] = quant_ydc(shape[0][0] >> 3) >> 1
+    CbCoeff[0] = quant_cdc(shape[1][0] >> 3)
+    CrCoeff[0] = quant_cdc(shape[2][0] >> 3)
+    
+    return (YCoeff, CbCoeff, CrCoeff)
+
+def cl_str(cY, cCb, cCr):
+    return "%sz%sz" % (
+        " ".join(cY[:NUM_Y].astype('str')),
+        " ".join(cCb[:NUM_C].astype('str')),
+        " ".join(cCr[:NUM_C].astype('str')))
+
+def cl_bithash(cY, cCb, cCr):
+    return histogram_hash(
+        numpy.concatenate(
+            cY, cCb, cCr).astype('double'))
+
+def cl_bithash_str(cY, cCb, cCr):
+    return histogram_hash_string(
+        numpy.concatenate(
+            cY, cCb, cCr).astype('double'))
+
+def cl_base64(cY, cCb, cCr):
+    pass
 
 def main(pth):
     from pylire.compatibility.utils import timecheck
@@ -140,9 +193,21 @@ def main(pth):
         print("image 'shape':")
         print(shape)
         print("")
+        print("image 'shape' shape:")
+        print(shape.shape)
+        print("")
 
     timetest_color_layout_shape(ndim)
 
+    @timecheck
+    def timetest_color_layout(ndim):
+        (cY, cCb, cCr) = color_layout(ndim)
+        print("")
+        # print(cY)
+        # print(cCb)
+        # print(cCr)
+
+    timetest_color_layout(ndim)
 
 if __name__ == '__main__':
 
